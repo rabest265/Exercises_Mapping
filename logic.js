42adc168753c379c9c9a5e2646712bb4d62adac2
Rabest265 <rabest265@gmail.com>
2019-06-14 10:26:59 -0400

    Completed map

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map").setView([40.8283, -75.5795], 5);
+var myMap = L.map("map").setView([38, -96], 4);
 
 // Add a tile layer (the background map image) to our map
 // Use the addTo method to add objects to our map
@@ -11,34 +11,60 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Create a red circle over Dallas
-L.circle([32.7767, -96.7970], {
-  color: "red",
-  fillColor: "red",
-  fillOpacity: 0.75,
-  radius: 50000
-}).addTo(myMap);
+function getColor(d) {
+  return d > 5 ? '#8B0000' :
+        d > 4  ? '#FF0000' :
+        d > 3  ? '#FF4500' :
+        d > 2  ? '#FF8C00' :
+        d > 1  ? '#FFD700' :
+                '#ADFF2F';
+}
 
-// Connect a black line from NYC to Toronto
-var line = [
-  [40.7128, -74.006],
-  [43.6532, -79.3832]
-];
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson" 
 
-// Create a polyline using the line coordinates and pass in some initial options
-L.polyline(line, {
-  color: "black"
-}).addTo(myMap);
+// / Grab the data with d3
+d3.json(url, function(response) {
 
+  console.log(response.features.length)
+  // Loop through data
+  for (var i = 0; i < response.features.length; i++) {
 
-// Create a purple polygon that covers the area in Atlanta, Savannah, Jacksonville and Montgomery
-L.polygon([
-  [33.749, -84.388],
-  [32.0809, -81.0912],
-  [30.3322, -81.6557],
-  [32.3792, -86.3077]
-], {
-  color: "purple",
-  fillColor: "purple",
-  fillOpacity: 0.75
-}).addTo(myMap);
+    // Set the data location property to a variable
+    var lng = response.features[i].geometry.coordinates[0];
+    var lat = response.features[i].geometry.coordinates[1];
+    var mag = response.features[i].properties.mag;
+    var place = response.features[i].properties.place;
+    // console.log(mag);
+
+  //   // Check for location property
+    if (lat) {
+      // Add a new marker to the cluster group and bind a pop-up
+      // console.log(lat);
+      L.circle([lat, lng],{
+        color: getColor(mag),
+        fillColor: getColor(mag),
+        fillOpacity: 0.75,
+        radius: mag*20000,
+      })
+      .bindPopup(("<h3>" + place + "</h3> <hr> <h3>Magnitude: " + mag + "</h3>"))
+      .addTo(myMap);
+    }
+  }
+});
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (myMap) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5]
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+        '<li class="square" style="background:' + getColor(grades[i]+1) + '">' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+') +'</li> ';
+  }
+  return div;
+};
+
+legend.addTo(myMap);
